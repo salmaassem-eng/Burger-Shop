1ffb33d11e9e6436ba0ec3c395233bf314de5ea1
SalmaAseem <salmaassem95@gmail.com>
2026-09-24 23:03:16 +0300

    finish slug meal and all details

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,36 @@
-export default function MealDetailPage() {
+import classes from './page.module.css';
+import Image from 'next/image';
+import { getMealBySlug } from '../../../lib/meals';
+import { notFound } from 'next/navigation';
+
+
+export default async function MealDetailPage({params}) {
+   const meal = await getMealBySlug(params.slug);
+    if(!meal) {
+      notFound();    }
+   meal.instructions = meal.instructions.replace(/\n/g, '<br>'); // Replace newline characters with <br> tags
     return (
+       <>
+        <header className={classes.header}>
+            <div className={classes.image}>
+                <Image src={meal.image} alt={meal.title} fill />
+             </div>
+             <div className={classes.headerText}>
+                <h1>{meal.title}</h1>
+                <p className={classes.creator}>
+                    by <a href={`mailto:${meal.creatorEmail}`}>{meal.creator}</a>
+                </p>
+                <p className={classes.summary}>
+                    {meal.summary}
+                </p>
+             </div>
+        </header>
         <main>
-            <h1 style={{ color: 'white', textAlign: 'center' }}>
-                Meal Detail Page
-            </h1>   
+            <p className={classes.instructions}
+            dangerouslySetInnerHTML={{ __html:meal.instructions }}
+            >
+            </p>
         </main>
+       </>
     );
 }
